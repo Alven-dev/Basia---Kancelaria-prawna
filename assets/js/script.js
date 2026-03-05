1a2e0e43031c3a7dc786d1d0d6cc1360cefd306e
Alven-dev <mikolajct@gmail.com>
2026-03-05 22:34:41 +0100

    some prep towards responsive display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,3 @@
-const boxes = document.querySelectorAll('.specialization-box');
-
-  boxes.forEach(box => {
-    const border = box.querySelector('.hover-border');
-
-    box.addEventListener('mouseenter', () => {
-      border.style.opacity = '1';
-      border.style.transform = 'scale(1)';
-    });
-
-    box.addEventListener('mouseleave', () => {
-      border.style.opacity = '0';
-      border.style.transform = 'scale(0.8)';
-    });
-  });
 
   document.querySelectorAll('.scroll-link').forEach(link => {
     link.addEventListener('click', function(e) {
@@ -26,3 +11,5 @@ const boxes = document.querySelectorAll('.specialization-box');
       });
     });
   });
+
+  
