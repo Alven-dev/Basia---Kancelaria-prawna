fa37d05354702fce598f804300bb183163a3787c
Alven-dev <mikolajct@gmail.com>
2026-03-11 23:13:16 +0100

    recreation of cta bg and some details improvements before moving on to next sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,11 +15,23 @@
   document.addEventListener("DOMContentLoaded", function (){
     const burger = document.querySelector(".burger");
     const navbar = document.getElementById("mobile-navbar");
+    const span = document.querySelectorAll(".burger span");
 
     burger.addEventListener("click", function() {
       burger.classList.toggle("active");
       navbar.classList.toggle("active");
+      colorSwap()
     });
-  });
+    
+    function colorSwap() {
+      span.forEach(function(el) {
+      if (burger.classList.contains("active")) {
+          el.style.backgroundColor = "#eaeaea";
+      } else {
+        el.style.backgroundColor = "#333"
+      }
+    });
+  }
+});
 
  
