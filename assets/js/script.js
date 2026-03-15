b38b80ef0c951d9819e8b5d81ac6bc64a63d51cc
Alven-dev <mikolajct@gmail.com>
2026-03-15 20:15:46 +0100

    almost finished product

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,31 +39,92 @@
     const modalClose = document.getElementById("modal-close");
     const modalContent = document.querySelector(".modal-content");
     const modalTitle = document.getElementById("modal-title");
-    const modalText = document.getElementById("modalText");
+    const modalText = document.getElementById("modal-text");
     const offerData = {
       rodzinne: {
         title: "Prawo Rodzinne",
-        text: "Pomoc prawna w sprawach z zakresu prawa rodzinnego, w szczególności prowadzenie spraw rozwodowych (zarówno z orzekaniem o winie, jak i bez), spraw o separację, alimenty oraz ich podwyższenie lub obniżenie. Reprezentacja w postępowaniach dotyczących władzy rodzicielskiej, ustalenia kontaktów z dzieckiem, ustalenia miejsca zamieszkania dziecka oraz ograniczenia lub pozbawienia władzy rodzicielskiej. Prowadzenie spraw o podział majątku wspólnego po rozwodzie, przygotowywanie pism procesowych, wniosków i pozwów oraz kompleksowa reprezentacja przed sądem rodzinnym."
+        list: [
+        "Prowadzenie spraw rozwodowych (z orzekaniem o winie lub bez)",
+        "Sprawy o separację",
+        "Sprawy o alimenty oraz ich podwyższenie lub obniżenie",
+        "Ustalenie kontaktów z dzieckiem",
+        "Ustalenie miejsca zamieszkania dziecka",
+        "Ograniczenie lub pozbawienie władzy rodzicielskiej",
+        "Podział majątku wspólnego po rozwodzie",
+        "Przygotowywanie pism procesowych, pozwów i wniosków",
+        "Reprezentacja przed sądem rodzinnym"
+        ]
       },
       cywilne: {
         title: "Prawo Cywilne",
-        text: "Prowadzenie spraw z zakresu prawa cywilnego, w tym dochodzenie roszczeń majątkowych, spraw o zapłatę oraz windykację należności. Pomoc w uzyskaniu odszkodowań i zadośćuczynień za poniesione szkody majątkowe i niemajątkowe. Sporządzanie, analiza i opiniowanie umów cywilnoprawnych, w tym umów sprzedaży, najmu czy umów o świadczenie usług. Reprezentacja klientów w sporach sądowych dotyczących nieruchomości, zobowiązań oraz innych spraw cywilnych, a także doradztwo prawne w zakresie praw i obowiązków wynikających z przepisów prawa cywilnego."
+        list: [
+        "Sprawy o zapłatę i dochodzenie roszczeń majątkowych",
+        "Windykacja należności",
+        "Odszkodowania i zadośćuczynienia za szkody majątkowe i niemajątkowe",
+        "Sporządzanie i analiza umów cywilnoprawnych",
+        "Umowy sprzedaży, najmu i umowy o świadczenie usług",
+        "Reprezentacja w sporach dotyczących nieruchomości",
+        "Reprezentacja w sporach dotyczących zobowiązań",
+        "Doradztwo prawne w zakresie prawa cywilnego"
+        ]
       },
       karne: {
         title: "Prawo Karne",
-        text: "Obrona osób podejrzanych i oskarżonych w sprawach karnych na każdym etapie postępowania – od postępowania przygotowawczego prowadzonego przez policję lub prokuraturę, aż po postępowanie sądowe. Reprezentacja osób pokrzywdzonych przestępstwem w charakterze oskarżyciela posiłkowego. Udział w przesłuchaniach, sporządzanie wniosków dowodowych, apelacji oraz innych środków odwoławczych. Pomoc w sprawach o przestępstwa gospodarcze, przestępstwa przeciwko mieniu, zdrowiu i życiu, a także obrona w sprawach o wykroczenia."
+        list: [
+        "Obrona osób podejrzanych i oskarżonych w sprawach karnych",
+        "Reprezentacja na etapie postępowania przygotowawczego",
+        "Reprezentacja przed sądem karnym",
+        "Reprezentacja pokrzywdzonych jako oskarżyciela posiłkowego",
+        "Udział w przesłuchaniach",
+        "Sporządzanie wniosków dowodowych",
+        "Przygotowywanie apelacji i środków odwoławczych",
+        "Sprawy o przestępstwa gospodarcze",
+        "Sprawy o przestępstwa przeciwko mieniu, zdrowiu i życiu",
+        "Obrona w sprawach o wykroczenia"
+        ]
       },
       przedsiębiorców: {
         title: "Prawo Przedsiębiorców",
-        text: "Kompleksowa obsługa prawna przedsiębiorców oraz osób prowadzących działalność gospodarczą. Pomoc w zakładaniu działalności gospodarczej oraz spółek, przygotowywanie i opiniowanie umów handlowych oraz innych dokumentów związanych z prowadzeniem biznesu. Reprezentacja przedsiębiorców w sporach z kontrahentami, w tym w sprawach o zapłatę oraz dochodzenie należności. Doradztwo prawne w zakresie bieżącego funkcjonowania firmy, identyfikacji ryzyk prawnych oraz przygotowywanie regulaminów i dokumentacji wymaganej w działalności gospodarczej."
+        list: [
+        "Kompleksowa obsługa prawna przedsiębiorców",
+        "Pomoc w zakładaniu działalności gospodarczej",
+        "Pomoc w zakładaniu spółek",
+        "Przygotowywanie i analiza umów handlowych",
+        "Tworzenie dokumentów związanych z prowadzeniem działalności",
+        "Reprezentacja przedsiębiorców w sporach z kontrahentami",
+        "Sprawy o zapłatę i dochodzenie należności",
+        "Doradztwo w zakresie bieżącego funkcjonowania firmy",
+        "Identyfikacja ryzyk prawnych w działalności gospodarczej",
+        "Przygotowywanie regulaminów i dokumentacji firmowej"
+        ]
       },
       medyczne: {
         title: "Prawo Medyczne",
-        text: "Pomoc prawna w sprawach dotyczących błędów medycznych oraz naruszenia praw pacjenta. Reprezentacja pacjentów w postępowaniach dotyczących odpowiedzialności cywilnej placówek medycznych oraz personelu medycznego. Analiza dokumentacji medycznej oraz wsparcie w dochodzeniu odszkodowań i zadośćuczynień za szkody powstałe w wyniku niewłaściwego leczenia. Doradztwo prawne na etapie przedsądowym oraz reprezentacja klientów w postępowaniach sądowych i przed właściwymi instytucjami."
+        list: [
+        "Sprawy dotyczące błędów medycznych",
+        "Sprawy dotyczące naruszenia praw pacjenta",
+        "Reprezentacja pacjentów w sporach z placówkami medycznymi",
+        "Reprezentacja w sporach z personelem medycznym",
+        "Analiza dokumentacji medycznej",
+        "Dochodzenie odszkodowań za błędy medyczne",
+        "Dochodzenie zadośćuczynień za szkody zdrowotne",
+        "Doradztwo prawne na etapie przedsądowym",
+        "Reprezentacja przed sądem i właściwymi instytucjami"
+        ]
       },
       mediacje: {
         title: "Mediacje",
-        text: "Prowadzenie mediacji jako alternatywnej metody rozwiązywania sporów, umożliwiającej osiągnięcie porozumienia bez konieczności długotrwałego postępowania sądowego. Pomoc w prowadzeniu mediacji w sprawach rodzinnych, cywilnych oraz gospodarczych. Wsparcie stron w wypracowaniu satysfakcjonującego rozwiązania konfliktu, przygotowanie projektów ugód mediacyjnych oraz udział w mediacjach prowadzonych na podstawie skierowania przez sąd lub z inicjatywy stron."
+        list: [
+        "Prowadzenie mediacji jako alternatywy dla postępowania sądowego",
+        "Mediacje w sprawach rodzinnych",
+        "Mediacje w sprawach cywilnych",
+        "Mediacje w sprawach gospodarczych",
+        "Pomoc w osiągnięciu porozumienia między stronami",
+        "Wsparcie w wypracowaniu satysfakcjonującego rozwiązania konfliktu",
+        "Przygotowywanie projektów ugód mediacyjnych",
+        "Mediacje prowadzone na podstawie skierowania przez sąd",
+        "Mediacje prowadzone z inicjatywy stron"
+        ]
       }
     }
 
@@ -71,8 +132,22 @@
     function openModal() {
       offerBox.forEach(function(card) {
         card.addEventListener("click", function() {
+
+          const type = this.dataset.offer;
+          const data = offerData[type];
+
           modal.style.display = "block";
-          modalTitle.innerText = this.dataset[offerData];
+
+          modalTitle.innerText = data.title
+          
+          modalText.innerHTML = ""
+
+          data.list.forEach(function(item) {
+            const li = document.createElement("li")
+            li.innerText = item
+
+            modalText.appendChild(li)
+          });
         })
       })
     }
